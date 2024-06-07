ddf9a3561b70ad48f3540af9f3e79df65f8547c2
Naeem <Naaeemmalik21@gmail.com>
2024-06-07 10:56:23 +0500

    replace link of brave with egobrowser from downlaods 'site is not secure,detail,why do i need this'

--- android/java/org/chromium/chrome/browser/BraveIntentHandler.java
+++ android/java/org/chromium/chrome/browser/BraveIntentHandler.java
@@ -28,7 +28,7 @@ public class BraveIntentHandler extends IntentHandler {
     private static final String CONNECTION_INFO_HELP_URL =
             "https://support.google.com/chrome?p=android_connection_info";
     private static final String BRAVE_CONNECTION_INFO_HELP_URL =
-            "https://support.brave.com/hc/en-us/articles/360018185871-How-do-I-check-if-a-site-s-connection-is-secure-";
+            "https://egonbrowser.com/";
 
     public BraveIntentHandler(Activity activity, IntentHandlerDelegate delegate) {
         super(activity, delegate);
